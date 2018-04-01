31975607da4599bd4ef017f40d2a6f805e66a533
Ty Miller <ty.loves.code@gmail.com>
2018-04-01 10:26:14 -0400

    added comment

--- middlewares/redirects.js
+++ middlewares/redirects.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const makeRedirectMiddleware = (redirectsMap) => (req, res, next) => {
+	//uses find method to serach for first entry in redirectsMap that matches
+	//search, in this case, requested path
   if (Object.keys(redirectsMap).find((entry) => entry === req.path)) {
     console.log(`Redirecting ${req.path} to ${redirectsMap[req.path]}`);
     res.redirect(301, redirectsMap[req.path]);
